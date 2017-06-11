ac04b113d1cdfb53ade65aa4dab1afe0e72ae3ab
bzzbzz7 <fyzy7@qq.com>
2017-06-11 22:25:17 +0800

    SpringIOC

---
+++ src/main/java/com/zz/java/frame/spring/README.txt
@@ -0,0 +1,13 @@
+
+Spring 控制反转和面向切面的容器框架
+IOC：
+AOP：
+
+
+SpringCore
+SpringContext
+SpringAOP
+SpringDAO
+SpringORM
+SpringWeb
+SpringMVC
---
+++ src/main/java/com/zz/java/frame/spring/chapter01/HelloChina.java
@@ -0,0 +1,13 @@
+package com.zz.java.frame.spring.chapter01;
+
+public class HelloChina implements IHelloMessage {
+
+	
+	//继承和实现了IHelloMessage接口中的sayHello方法，
+	//向大家输出了Hello China的信息
+	@Override
+	public String sayHello() {
+
+		return "大家好!";
+	}
+}
